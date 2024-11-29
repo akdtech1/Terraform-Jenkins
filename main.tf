377be78546c7e39368d77e0293d9d9cddf6be421
akdtech1 <anandkumardeva@gmail.com>
2024-11-30 00:40:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-0866a3c8686eaeeba" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "JenkinsTerraForm-Instance"
   }
 }
